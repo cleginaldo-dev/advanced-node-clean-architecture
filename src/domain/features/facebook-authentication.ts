c4e9193a09f17d9c65ef52227ec1260b2aa1321f
Cleginaldo <cleginaldo3000@gmail.com>
2023-01-28 15:38:36 -0300

    feat: add facebook authentication frature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,13 @@
+import { AuthenticationError } from '@/domain/errors';
+import { AccessToken } from '@/domain/models';
+
+export interface IFacebookAuthentication {
+  perform: (
+    params: IFacebookAuthentication.Params,
+  ) => Promise<IFacebookAuthentication.Result>;
+}
+
+export namespace IFacebookAuthentication {
+  export type Params = { token: string };
+  export type Result = AccessToken | AuthenticationError;
+}
