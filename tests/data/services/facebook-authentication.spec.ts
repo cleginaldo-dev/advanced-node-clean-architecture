62354e9b988428b9d91226f29b430dd3257dad80
Cleginaldo <cleginaldo3000@gmail.com>
2023-01-28 23:35:23 -0300

    feat: simplify code

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -6,6 +6,7 @@ import { mock, MockProxy } from 'jest-mock-extended';
 describe('FacebookAuthentication', () => {
   let loadFacebookUserApi: MockProxy<ILoadFacebookUserApi>;
   let sut: FacebookAuthenticationService;
+  const token = 'any_token';
 
   beforeEach(() => {
     loadFacebookUserApi = mock();
@@ -13,18 +14,16 @@ describe('FacebookAuthentication', () => {
   });
 
   it('Should be able call LoadFacebookUserApi with correct params', async () => {
-    await sut.perform({ token: 'any_token' });
+    await sut.perform({ token });
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({
-      token: 'any_token',
-    });
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token });
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
   });
 
   it('Should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
 
-    const authResult = await sut.perform({ token: 'any_token' });
+    const authResult = await sut.perform({ token });
 
     expect(authResult).toEqual(new AuthenticationError());
   });
