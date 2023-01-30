6483370735bdd08c2a92b658cff1776732eda14e
Cleginaldo <cleginaldo3000@gmail.com>
2023-01-30 16:38:45 -0300

    feat: move business role to a domain entity

---
+++ tests/domain/models/facebook-account.spec.ts
@@ -0,0 +1,44 @@
+import { FacebookAccount } from '@/domain/models/facebook-account';
+
+describe('FacebookAccount', () => {
+  const fbData = {
+    name: 'any_fb_name',
+    email: 'any_fb_email',
+    facebook_id: 'any_facebook_id',
+  };
+  it('Should create with facebook data only', () => {
+    const sut = new FacebookAccount(fbData);
+
+    expect(sut).toEqual({
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebook_id: 'any_facebook_id',
+    });
+  });
+
+  it('Should update name if its empty', () => {
+    const accountData = { id: 'any_id' };
+
+    const sut = new FacebookAccount(fbData, accountData);
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebook_id: 'any_facebook_id',
+    });
+  });
+
+  it('Should not update name if its not empty', () => {
+    const accountData = { id: 'any_id', name: 'any_name' };
+
+    const sut = new FacebookAccount(fbData, accountData);
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_fb_email',
+      facebook_id: 'any_facebook_id',
+    });
+  });
+});
