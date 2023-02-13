9a2ebed33f48576d0a6556335788d8025b2ff195
Cleginaldo <cleginaldo3000@gmail.com>
2023-02-12 23:16:11 -0300

    feat: ensure FacebookApi get app token with correct params

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -3,10 +3,19 @@ import { mock, MockProxy } from 'jest-mock-extended';
 
 export class FacebookApi {
   private readonly baseUrl = 'https://graph.facebook.com';
-  constructor(private readonly httpClient: IHttpGetClient) {}
+  constructor(
+    private readonly httpClient: IHttpGetClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string,
+  ) {}
   async loadUser(params: ILoadFacebookUserApi.Params): Promise<void> {
     await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: 'client_credentials',
+      },
     });
   }
 }
@@ -18,21 +27,33 @@ export interface IHttpGetClient {
 export namespace IHttpGetClient {
   export type Params = {
     url: string;
+    params: {
+      client_id: string;
+      client_secret: string;
+      grant_type: string;
+    };
   };
 }
 
 describe('FacebookApi', () => {
   let httpClient: MockProxy<IHttpGetClient>;
+  const clientId = 'any_client_id';
+  const clientSecret = 'any_client_secret';
   beforeAll(() => {
     httpClient = mock();
   });
   it('Should get app token', async () => {
-    const sut = new FacebookApi(httpClient);
+    const sut = new FacebookApi(httpClient, clientId, clientSecret);
 
-    await sut.loadUser({ token: 'any_clent_token' });
+    await sut.loadUser({ token: 'any_client_token' });
 
     expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/oauth/access_token',
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials',
+      },
     });
   });
 });
