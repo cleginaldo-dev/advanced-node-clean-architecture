2b4258962937f40ad71fafedcb04463153822a1e
Cleginaldo <cleginaldo3000@gmail.com>
2023-04-20 00:14:03 -0300

    Should be able call get with correct values

---
+++ tests/infra/http/axios-client.spec.ts
@@ -0,0 +1,33 @@
+import { IHttpGetClient } from '@/infra/http';
+import axios from 'axios';
+
+jest.mock('axios');
+
+class AxiosHttpClient {
+  async get(args: IHttpGetClient.Params): Promise<void> {
+    await axios.get(args.url, {
+      params: args.params,
+    });
+  }
+}
+
+describe('AxiosHttpClient', () => {
+  describe('get', () => {
+    it('Should be able call get with correct values', async () => {
+      const fakeAxios = axios as jest.Mocked<typeof axios>;
+      const sut = new AxiosHttpClient();
+
+      await sut.get({
+        url: 'any_url',
+        params: {
+          any: 'any',
+        },
+      });
+
+      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
+        params: { any: 'any' },
+      });
+      expect(fakeAxios.get).toHaveBeenCalledTimes(1);
+    });
+  });
+});
