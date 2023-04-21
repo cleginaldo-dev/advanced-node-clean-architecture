7d874bb5ed61ccf056cdf8de9503eff7dc12ee52
Cleginaldo <cleginaldo3000@gmail.com>
2023-04-20 23:52:28 -0300

    Should be able call sign of jwtTokenGenerator with correct values

---
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -0,0 +1,38 @@
+import { ITokenGenerator } from '@/data/contracts/crypto/token';
+import jwt from 'jsonwebtoken';
+
+jest.mock('jsonwebtoken');
+
+class JwtTokenGenerator {
+  constructor(private readonly secret: string) {}
+  async generateToken(params: ITokenGenerator.Params): Promise<void> {
+    const expirationInSeconds = params.expirationInMs / 1000;
+    jwt.sign({ key: params.key }, this.secret, {
+      expiresIn: expirationInSeconds,
+    });
+  }
+}
+
+describe('JwtTokenGenerator', () => {
+  let sut: JwtTokenGenerator;
+  let fakeJwt: jest.Mocked<typeof jwt>;
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>;
+  });
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator('any_secret');
+  });
+
+  it('Should be able call sign with correct values', async () => {
+    await sut.generateToken({ key: 'any_key', expirationInMs: 1000 });
+
+    expect(fakeJwt.sign).toHaveBeenCalledWith(
+      { key: 'any_key' },
+      'any_secret',
+      {
+        expiresIn: 1,
+      },
+    );
+  });
+});
