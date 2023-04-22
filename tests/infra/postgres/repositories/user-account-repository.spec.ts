384e82d882732427b26dcb284d193c3cde786564
Cleginaldo <cleginaldo3000@gmail.com>
2023-04-21 23:15:16 -0300

    move code creation to its own file

--- tests/infra/postgres/repositories/user-account-repository.spec.ts
+++ tests/infra/postgres/repositories/user-account-repository.spec.ts
@@ -1,54 +1,15 @@
-import { ILoadUserAccountRepository } from '@/data/contracts/repositories';
-import { IBackup, newDb } from 'pg-mem';
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  getRepository,
-  Repository,
-  getConnection,
-} from 'typeorm';
+import { PgUser } from '@/infra/postgres/entities';
+import { PgUserAccountRepository } from '@/infra/postgres/repositories';
+import { makeFackDb } from '@/tests/infra/postgres/mocks';
+import { IBackup } from 'pg-mem';
+import { getRepository, Repository, getConnection } from 'typeorm';
 
-@Entity({ name: 'usuarios' })
-export class PgUser {
-  @PrimaryGeneratedColumn()
-  id!: number;
-
-  @Column({ name: 'nome', nullable: true })
-  name?: string;
-
-  @Column()
-  email!: string;
-
-  @Column({ name: 'id_facebook', nullable: true })
-  facebookId?: string;
-}
-
-class PgUserAccountRepository implements ILoadUserAccountRepository {
-  async load(
-    params: ILoadUserAccountRepository.Params,
-  ): Promise<ILoadUserAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser);
-    const pgUser = await pgUserRepo.findOne({ where: { email: params.email } });
-    if (pgUser) {
-      return {
-        id: pgUser.id.toString(),
-        name: pgUser.name ?? undefined,
-      };
-    }
-  }
-}
 describe('PgUserAccountRepository', () => {
   let sut: PgUserAccountRepository;
   let pgUserRepo: Repository<PgUser>;
   let backup: IBackup;
   beforeAll(async () => {
-    const db = newDb();
-    const connection = await db.adapters.createTypeormConnection({
-      type: 'postgres',
-      entities: [PgUser],
-    });
-    await connection.synchronize();
+    const db = await makeFackDb();
     backup = db.backup();
     pgUserRepo = getRepository(PgUser);
   });
@@ -70,8 +31,6 @@ describe('PgUserAccountRepository', () => {
     expect(account).toEqual({ id: '1' });
   });
   it('Should return undefined if email does not exists', async () => {
-    const sut = new PgUserAccountRepository();
-
     const account = await sut.load({ email: 'new_email' });
 
     expect(account).toBeUndefined();
