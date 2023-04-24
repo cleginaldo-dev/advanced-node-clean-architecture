7385442ed0a7acdf69629bb0ed63ceaeae17ce3a
Cleginaldo <cleginaldo3000@gmail.com>
2023-04-23 22:01:24 -0300

    ensure RequiredStringValidator returns error if value is empty

---
+++ tests/application/validation/required-string-validator.spec.ts
@@ -0,0 +1,39 @@
+import { RequiredFieldError } from '@/application/errors';
+
+class RequiredStringValidator {
+  constructor(
+    private readonly value: string,
+    private readonly fieldName: string,
+  ) {}
+
+  validate(): Error | undefined {
+    return new RequiredFieldError(this.fieldName);
+  }
+}
+describe('RequiredStringValidator', () => {
+  beforeAll(() => {});
+
+  beforeEach(() => {});
+
+  it('Should return RequiredFieldError if value is empty', async () => {
+    const sut = new RequiredStringValidator('', 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredFieldError('any_field'));
+  });
+  it('Should return RequiredFieldError if value is null', async () => {
+    const sut = new RequiredStringValidator(null as any, 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredFieldError('any_field'));
+  });
+  it('Should return RequiredFieldError if value is undefined', async () => {
+    const sut = new RequiredStringValidator(undefined as any, 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredFieldError('any_field'));
+  });
+});
