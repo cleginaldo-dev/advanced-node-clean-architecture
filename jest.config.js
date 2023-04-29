0355f3accc28a202d4a0508771a84db187ae6c44
Cleginaldo <cleginaldo3000@gmail.com>
2023-04-29 15:40:46 -0300

    add jest config to integration tests

--- jest.config.js
+++ jest.config.js
@@ -1,44 +1,27 @@
 module.exports = {
-
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/index.ts',
   ],
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-
-  // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
     "/node_modules/"
   ],
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "babel",
-
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1',
   },
-
-
-  // A list of paths to directories that Jest should use to search for files in
+  testMatch: [
+    '**/*.spec.ts'
+  ],
   roots: [
     "<rootDir>/src",
     "<rootDir>/tests",
   ],
-
-
-
-  // A map from regular expressions to paths to transformers
   transform: {
     '\\.ts$': 'ts-jest'
   },
-
-  // Clear mocks before each test
   clearMocks: true,
 };
---
+++ tests/external/facebook-api.test.ts
@@ -0,0 +1,9 @@
+describe('FacebookApi', () => {
+  beforeAll(() => {});
+
+  beforeEach(() => {});
+
+  it('Should be able call Repository with correct values', async () => {
+    expect(1).toBe(1);
+  });
+});
