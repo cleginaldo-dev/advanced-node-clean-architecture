6cdd7d487668fd8e5ee343b30d025ff899f86f15
Cleginaldo <cleginaldo3000@gmail.com>
2023-05-04 00:23:44 -0300

    fix

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,8 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '255017709763229',
+    clientId: process.env.FB_CLIENT_ID ?? '237318072210729',
     clientSecret:
-      process.env.FB_CLIENT_SECRET ?? 'c02b1e197ee6aec2947b193148cdaef7',
+      process.env.FB_CLIENT_SECRET ?? '68d011ee0417c2e9a6ebb874a081294a',
   },
 };
+// https://manysex.com/pt/video/1505503/bitchselena-ass-to-mouth-in-rough-anal-sex-on-the-couch/?promo=31134
