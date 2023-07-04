5289bc550a9baeea9ec25d0072f6b9127c62baaf
Cleginaldo <cleginaldo3000@gmail.com>
2023-07-03 22:38:22 -0300

    ensure ExpressRoute responds with 200 and correct data

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -6,8 +6,9 @@ import { mock, MockProxy } from 'jest-mock-extended';
 export class ExpressRouter {
   constructor(private readonly controler: Controller) {}
 
-  public async adapt(req: Request, _res: Response): Promise<void> {
-    await this.controler.handle({ ...req.body });
+  public async adapt(req: Request, res: Response): Promise<void> {
+    const httpResponse = await this.controler.handle({ ...req.body });
+    res.status(200).json(httpResponse.data);
   }
 }
 
@@ -20,6 +21,10 @@ describe('ExpressRouter', () => {
     req = getMockReq({ body: { any: 'any' } });
     res = getMockRes().res;
     controller = mock();
+    controller.handle.mockResolvedValue({
+      statusCode: 200,
+      data: { data: 'any_data' },
+    });
   });
 
   beforeEach(() => {
@@ -37,6 +42,13 @@ describe('ExpressRouter', () => {
     await sut.adapt(req, res);
 
     expect(controller.handle).toHaveBeenCalledWith({});
+    expect(controller.handle).toHaveBeenCalledTimes(1);
+  });
+  it('Should respond with 200 and correct data', async () => {
+    await sut.adapt(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ data: 'any_data' });
     expect(controller.handle).toHaveBeenCalledTimes(1);
   });
 });
