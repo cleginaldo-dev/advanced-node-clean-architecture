ba329ee830500fb1715402b92e28e10675d21df8
Cleginaldo <cleginaldo3000@gmail.com>
2023-08-03 08:58:33 -0300

    feat: change fb client secret

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,7 +2,7 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '2465464003623649',
     clientSecret:
-      process.env.FB_CLIENT_SECRET ?? '101f38721d356404b0bc4521eb23684e'
+      process.env.FB_CLIENT_SECRET ?? '84c5ceb01a6df05bd5a35aac09a40d72'
   },
   port: process.env.PORT ?? 4444,
   jwtSecret: process.env.JWT_SECRET ?? 'k90df8hd0f808dfg8d09'
