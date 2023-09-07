1d1ca25cb72653dd75d212516528e114ef5f42fc
Cleginaldo <cleginaldo3000@gmail.com>
2023-09-07 08:32:47 -0300

    feat: ensure return correct uuid

---
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -0,0 +1,31 @@
+import { IUuidGenerator } from '@/domain/contracts/gateways'
+import { v4 } from 'uuid'
+
+export class UuidHandler implements IUuidGenerator {
+  uuid({ key }: IUuidGenerator.Input): IUuidGenerator.Output {
+    return `${key}_${v4()}`
+  }
+}
+
+jest.mock('uuid')
+describe('UuidHandler', () => {
+  let sut: UuidHandler
+  beforeAll(() => {})
+
+  beforeEach(() => {
+    sut = new UuidHandler()
+  })
+
+  it('Should call uuid.v4', () => {
+    sut.uuid({ key: 'any_key' })
+
+    expect(v4).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return correct uuid', () => {
+    jest.mocked(v4).mockReturnValueOnce('any_uuid')
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_any_uuid')
+  })
+})
